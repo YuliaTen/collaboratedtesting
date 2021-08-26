bc01affb75f001b36fc6ab1feaa194fac6560161
Aleksandr Kuzmichev <d5f@ya.ru>
2021-08-26 21:11:35 +0300

    Created first market cap test.

--- src/test/java/ru/devray/day13/collaboratedtesting/MarketCapTest.java
+++ src/test/java/ru/devray/day13/collaboratedtesting/MarketCapTest.java
@@ -1,14 +1,34 @@
 package ru.devray.day13.collaboratedtesting;
 
+import io.restassured.response.Response;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
 
 public class MarketCapTest {
 
     final static String TOKEN = "53ef908ec640538fc602f353f474534a";
-    final static String URL = "https://financialmodelingprep.com/api/v3/market-capitalization/AAPL?apikey=YOUR_API_KEY";
+    final static String URI = "https://financialmodelingprep.com/api/v3";
+    final static String PATH = "/market-capitalization";
 
     @Test
+    @DisplayName("Проверка корректности возвращаемых данных рыночной капитализации по акции")
     void testMarketCapitalization(){
+
+        given()
+                .baseUri(URI)
+                .basePath(PATH)
+                .log().all()
+                .queryParam("apikey", TOKEN)
+                        .when().get("/AAPL")
+                                .then().log().all()
+                        .body("[0].symbol", Matchers.equalTo("AAPL"))
+                        .body("[0].date", Matchers.matchesRegex("[\\w-]+"))
+                        .body("[0].marketCap", Matchers.notNullValue());
+
 
     }
 
